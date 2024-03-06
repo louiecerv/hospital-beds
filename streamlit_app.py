4bb94e919b4ad04564120bbc38dc1c40794ff3ef
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-06 08:24:11 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -262,7 +262,7 @@ def display_form3():
         form3.write(test_data_scaled)
 
         predicted =  st.session_state["clf"].predict(test_data_scaled)
-        result = 'Will the debtor pay? The model predicts: ' + predicted[0]
+        result = 'Will the debtor full pay the loan? The model predicts: ' + predicted[0]
         form3.subheader(result)
 
     submit3 = form3.form_submit_button("Reset")
