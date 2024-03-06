f14692ca1f39ed5156d71c2ca9a687e4b1896e7c
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-06 15:40:04 +0800

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -322,7 +322,7 @@ def display_form3():
         form3.write(test_data_scaled)
 
         predicted =  st.session_state["clf"].predict(test_data_scaled)
-        result = "How many hospital beds are needed? The model predicts: \n" +  f"{predicted[0]:,.0f}" 
+        result = "The model predicts the number of beds needed: " +  f"{predicted[0]:,.0f}" 
         form3.subheader(result)
 
     submit3 = form3.form_submit_button("Reset")
